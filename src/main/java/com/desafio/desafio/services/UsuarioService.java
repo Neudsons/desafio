151a6e302a84116b36cbbd8952ac50b76b7cffd6
Neudsons <neudsons@gmail.com>
2020-02-16 15:21:08 -0300

    Exception Update

--- src/main/java/com/desafio/desafio/services/UsuarioService.java
+++ src/main/java/com/desafio/desafio/services/UsuarioService.java
@@ -3,6 +3,8 @@ package com.desafio.desafio.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +46,13 @@ public class UsuarioService {
 	}
 	
 	public Usuario update(Long id, Usuario obj) {
+		try {
 		Usuario entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
 	}
 
